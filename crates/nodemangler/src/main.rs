9e16215b49e2678b9ad576cf639c22b11576c1c7
Dan <danphi@gmail.com>
2023-05-08 01:54:45 -0400

    asdf

--- crates/nodemangler/src/main.rs
+++ crates/nodemangler/src/main.rs
@@ -1,9 +1,10 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")] // hide console window on Windows in release
 
+use eframe::egui;
+use eframe::epaint::Rounding;
 use mangler::graph::Graph;
 use mangler::nodes::*;
 use mangler::value::Value;
-use eframe::egui;
 
 const WINDOW_WIDTH: f32 = 1280.0;
 const WINDOW_HEIGHT: f32 = 800.0;
@@ -11,7 +12,7 @@ pub const MIN_NODE_SIZE: [f32; 2] = [200.0, 200.0];
 
 fn main() -> Result<(), eframe::Error> {
     // let mut graph = Graph::new();
-    
+
     // let id = add::Add::new(&mut graph);
 
     // if let Some(node) = graph.nodes.get_mut(&id) {
@@ -22,7 +23,7 @@ fn main() -> Result<(), eframe::Error> {
 
     // if let Some(v) = graph.nodes.get(&id) {
     //     println!("Hello, world! {:?}", v.print_output());
-    // } 
+    // }
 
     let options = eframe::NativeOptions {
         initial_window_size: Some(egui::vec2(WINDOW_WIDTH, WINDOW_HEIGHT)),
@@ -52,68 +53,45 @@ impl Default for MyApp {
 
 impl eframe::App for MyApp {
     fn update(&mut self, ctx: &egui::Context, _frame: &mut eframe::Frame) {
-        
         egui::CentralPanel::default().show(ctx, |ui| {
-            // ui.heading("My egui Application");
-            // ui.horizontal(|ui| {
-            //     let name_label = ui.label("Your name: ");
-            //     ui.text_edit_singleline(&mut self.name)
-            //         .labelled_by(name_label.id);
-            // });
-            // ui.add(egui::Slider::new(&mut self.age, 0..=120).text("age"));
-            // if ui.button("Click each year").clicked() {
-            //     self.age += 1;
-            // }
-            // ui.label(format!("Hello '{}', age {}", self.name, self.age));
-            // egui::TopBottomPanel::top("top_panel")
-            //     .resizable(true)
-            //     .min_height(32.0)
-            //     .show_inside(ui, |ui| {
-            //         egui::ScrollArea::vertical().show(ui, |ui| {
-            //             ui.vertical_centered(|ui| {
-            //                 ui.heading("Expandable Upper Panel");
-            //             });
-            //             //lorem_ipsum(ui);
-            //         });
-            //     });
-
             egui::SidePanel::left("left_panel")
-                .default_width(150.0)
+                .default_width(200.0)
                 .resizable(false)
-                //.default_width(150.0)
-                //.width_range(80.0..=200.0)
                 .show_inside(ui, |ui| {
-                    // ui.vertical_centered(|ui| {
-                    //     ui.heading("Left Panel");
-                    // });
                     egui::ScrollArea::vertical().show(ui, |ui| {
-                        //lorem_ipsum(ui);
+                        if ui.add(egui::Button::new("asdf")).clicked() {
+                            println!("clicked");
+                        }
                     });
                 });
-
-            // egui::SidePanel::right("right_panel")
-            //     .resizable(true)
-            //     .default_width(150.0)
-            //     .width_range(80.0..=200.0)
-            //     .show_inside(ui, |ui| {
-            //         ui.vertical_centered(|ui| {
-            //             ui.heading("Right Panel");
-            //         });
-            //         egui::ScrollArea::vertical().show(ui, |ui| {
-            //             //lorem_ipsum(ui);
-            //         });
-            //     });
 
             egui::TopBottomPanel::bottom("bottom_panel")
                 .resizable(true)
                 //.min_height(50.0)
                 .default_height(WINDOW_HEIGHT / 2.0)
                 .show_inside(ui, |ui| {
-                    // ui.vertical_centered(|ui| {
-                    //     ui.heading("Bottom Panel");
-                    // });
+                    ui.set_clip_rect(ui.max_rect());
+                    let r = ui.allocate_rect(
+                        ui.min_rect(),
+                        egui::Sense::click().union(egui::Sense::drag()),
+                    );
+
+                    if r.clicked() {
+                        println!("clicked");
+                    } else if r.drag_started() {
+                        println!("drag started");
+                    } else if r.drag_released() {
+                        println!("drag released");
+                    }
+
+                    ui.vertical_centered(|ui| {
+                        let node = GraphNode {
+                            position: egui::Pos2::new(0.0, WINDOW_HEIGHT * 0.6),
+                        };
+                        node.show(ui);
+                    });
                     let editor_rect = ui.max_rect();
-                    ui.allocate_rect(editor_rect, egui::Sense::hover());                     
+                    ui.allocate_rect(editor_rect, egui::Sense::hover());
                 });
 
             egui::CentralPanel::default().show_inside(ui, |ui| {
@@ -128,27 +106,34 @@ impl eframe::App for MyApp {
     }
 }
 
-
 pub struct GraphNode {
-    pub position: egui::Vec2
+    pub position: egui::Pos2,
 }
 
 impl GraphNode {
     pub fn show(self, ui: &mut egui::Ui) {
-        ui.child_ui_with_id_source(
-            egui::Rect::from_min_size(
-                egui::Pos2::new(0.0, 0.0),
-                MIN_NODE_SIZE.into(),
-            ),
+        ui.painter().add(egui::Shape::rect_filled(
+            egui::Rect::from_two_pos(self.position, egui::pos2(200.0, 200.0)),
+            Rounding::same(4.0),
+            egui::Color32::from_gray(70),
+        ));
+        ui.child_ui(
+            egui::Rect::from_min_max(egui::Pos2::new(0.0, 0.0), egui::Pos2::new(100.0, 100.0)),
             egui::Layout::default(),
-            0
-        ).vertical(ui, |ui| {
-            ui.add(Label::new(
-                RichText::new(&self.graph[self.node_id].label)
-                    .text_style(TextStyle::Button)
-                    .color(text_color),
-            ));
-            ui.add_space(8.0);
+        )
+        .vertical(|ui| {
+            ui.child_ui_with_id_source(
+                egui::Rect::from_min_size(egui::Pos2::new(0.0, 0.0), MIN_NODE_SIZE.into()),
+                egui::Layout::default(),
+                0,
+            )
+            .vertical(|ui| {
+                ui.add(egui::Label::new(
+                    egui::RichText::new("asdf")
+                        .text_style(egui::TextStyle::Button)
+                        .color(egui::Color32::RED),
+                ));
+            });
         });
     }
 }
